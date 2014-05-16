932b7bc97e0b563423c5ad8b920e7bd320f305ae
mraab89 <mario.raab89@web.de>
2014-05-16 23:39:43 +0200

    method getPriceCode + setPriceCode updated

--- src/Movie.java
+++ src/Movie.java
@@ -7,14 +7,31 @@ public class Movie {
     
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode (priceCode);
     }
+    
     public int getPriceCode() {
-        return priceCode;
-    }
+    	return _price.getPriceCode();
+    	}
+    
     public void setPriceCode(int arg) {
-        priceCode = arg;
+    	switch (arg) {
+    		case REGULAR:
+    			_price = new RegularPrice();
+    			break;
+    		case CHILDRENS:
+    			_price = new ChildrensPrice();
+    			break;
+    		case NEW_RELEASE:
+    			_price = new NewReleasePrice();
+    			break;
+    		default:
+    			throw new IllegalArgumentException("Incorrect Price Code");
+    	}
     }
+    
+    private Price _price;
+   
     public String getTitle (){
         return title;
     };
