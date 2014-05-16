663f6d649ca91c0b79f7765e54308205e5d70d46
mraab89 <mario.raab89@web.de>
2014-05-16 19:05:44 +0200

    Replace Temp with Query in class Customer

--- src/Customer.java
+++ src/Customer.java
@@ -22,10 +22,9 @@ class Customer {
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
+          
             Rental each = (Rental) enum_rentals.nextElement();
             
-            thisAmount = each.getCharge();
             
             // add frequent renter points
             frequentRenterPoints ++;
@@ -33,8 +32,8 @@ class Customer {
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
                 frequentRenterPoints ++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
