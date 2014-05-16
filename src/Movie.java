19e8d3fd1a469c62ff6ef27eb7a648bab9fd5b97
mraab89 <mario.raab89@web.de>
2014-05-16 23:51:38 +0200

    move method to getCharge

--- src/Movie.java
+++ src/Movie.java
@@ -36,23 +36,8 @@ public class Movie {
         return title;
     };
     
-    double getCharge() {
-    	double result = 0;
-    	switch (getPriceCode()) {
-    		case Movie.REGULAR:
-    			result += 2;
-    			if (getDaysRented() > 2)
-    				result += (getDaysRented() - 2) * 1.5;
-    		break;
-    		case Movie.NEW_RELEASE:
-    			result += getDaysRented() * 3;
-    			break;
-    		case Movie.CHILDRENS:
-    			result += 1.5;
-    			if (getDaysRented() > 3)
-    				result += (getDaysRented() - 3) * 1.5;
-    			break;
-    	}
+    double getCharge(int daysRented) {
+    	return _price.getCharge(daysRented);
     }
     	
     int getFrequentRenterPoints(int daysRented) {
