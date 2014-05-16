4353c9daf9740856b144b41a61b7038ca648fb8b
mraab89 <mario.raab89@web.de>
2014-05-16 23:16:37 +0200

    class Movie updated

--- src/Movie.java
+++ src/Movie.java
@@ -4,6 +4,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String title;
     private int priceCode;
+    
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         priceCode = newpriceCode;
@@ -17,4 +18,30 @@ public class Movie {
     public String getTitle (){
         return title;
     };
+    
+    double getCharge() {
+    	double result = 0;
+    	switch (getPriceCode()) {
+    		case Movie.REGULAR:
+    			result += 2;
+    			if (getDaysRented() > 2)
+    				result += (getDaysRented() - 2) * 1.5;
+    		break;
+    		case Movie.NEW_RELEASE:
+    			result += getDaysRented() * 3;
+    			break;
+    		case Movie.CHILDRENS:
+    			result += 1.5;
+    			if (getDaysRented() > 3)
+    				result += (getDaysRented() - 3) * 1.5;
+    			break;
+    	}
+    }
+    	
+    int getFrequentRenterPoints(int daysRented) {
+    	if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+    		return 2;
+    	else
+    		return 1;
+    }
 }
