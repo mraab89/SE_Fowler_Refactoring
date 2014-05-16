0610b048a5f9aab60b4ac5bf956cfd2b073471e4
mraab89 <mario.raab89@web.de>
2014-05-16 22:24:22 +0200

    method frequentRenterPoints

--- src/Rental.java
+++ src/Rental.java
@@ -31,4 +31,12 @@ class Rental {
     	}
     	return result;
     }
+    
+	public int getFrequentRenterPoints() {
+		// TODO Auto-generated method stub
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
+		    return 2;
+		else 
+			return 1;
+	}
 }
